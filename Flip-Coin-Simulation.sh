1fa8034b8b5b2ae03d8db841d9dd2afb876714de
dksaini70 <dkreview70@gmail.com>
2021-06-27 08:46:08 +0530

    simulating till head or tail come 21time

--- Flip-Coin-Simulation.sh
+++ Flip-Coin-Simulation.sh
@@ -1,8 +1,11 @@
+#!/bin/bash 
 echo "Simulator start with Flipping a Coin to Display Heads or Tails"
 Tail=0
 Head=1
-
-for ((i=1;i<=10;i++))
+won=20
+count=0
+count1=0
+while [[ $count -le $won && $count1 -le $won  ]]
 do
 coin=$((RANDOM%2))
 if [ $coin -eq $Head ]
@@ -14,5 +17,16 @@ echo "Tail"
 ((count1++))
 fi
 done
-echo "no. of head win "$count
-echo "no. of tail win"$count1
+echo $count
+echo $count1
+
+if [ $count -gt $count1 ]
+then
+echo "head win "$count
+elif [ $count1 -gt $count ]
+then
+echo "tail is win"$count1
+elif [ $count -eq $count1 ]
+then
+echo "game is tie"
+fi
