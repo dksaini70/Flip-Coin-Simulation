23036fd0139d212b591d926eaa156bd10e744c1e
dksaini70 <dkreview70@gmail.com>
2021-06-27 08:12:42 +0530

    no. of wining of head or tail

--- Flip-Coin-Simulation.sh
+++ Flip-Coin-Simulation.sh
@@ -1,10 +1,18 @@
 echo "Simulator start with Flipping a Coin to Display Heads or Tails"
-coin=$((RANDOM%2))
 Tail=0
 Head=1
+
+for ((i=1;i<=10;i++))
+do
+coin=$((RANDOM%2))
 if [ $coin -eq $Head ]
 then
 echo "Head"
+((count++))
 else
 echo "Tail"
+((count1++))
 fi
+done
+echo "no. of head win "$count
+echo "no. of tail win"$count1
